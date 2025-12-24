68ddebae82100b2b84fbd39d80a62938ef9394e4
SunilYanamala18 <sunil.yanamala@Covasant.com>
2025-12-24 11:18:11 +0530

    Delete assignment1.py

--- assignment1.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-input_str="Hello world and Hello Earth"
-input_str=input_str.lower()
-listofwords=input_str.split()
-print(listofwords)
-wordcount=Counter(listofwords)
-print(wordcount)
-for w,c in wordcount.items():
-    print(f"{w},{c}")
